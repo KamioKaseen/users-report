8483daf0d2c1db4adc1c5ab345f849ae945c5578
Dmitry Kondratenko <craigbune@gmail.com>
2025-03-16 00:24:51 +0300

    feat: добавил анимацию загрузки карточек + api

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -2,20 +2,22 @@ import './styles.scss';
 import { ReactComponent as Phone } from "@/assets/icons/phone.svg";
 import { ReactComponent as Mail } from "@/assets/icons/mail.svg";
 
-export default function temp() {
+
+
+export default function UserCard({name, phone, email}) {
   return (
     <div className="user-card">
-      <h2 className='user-card__title'>Евгения Савченко</h2>
+      <h2 className='user-card__title'>{name}</h2>
 
       <div className="user-card__contacts">
         <div className="user-card__icons-container">
           <Phone width={24} height={24}/>
-          <a href="tel:+1234567890">+7 (918) 078-17-05</a>  
+          <a href="tel:+1234567890">{phone}</a>  
         </div>
         
         <div className="user-card__icons-container">
           <Mail width={24} height={24}/>
-          <a href="mailto:example@example.com">yysavchenk@mail.ru</a>  
+          <a href="mailto:example@example.com">{email}</a>  
         </div>
       </div>
     </div>
